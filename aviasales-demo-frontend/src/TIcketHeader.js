677e87dd9eef653acf665c49cf0b5b7e0bb4f5fd
a.prostyakova@rain.wtf <a.prostyakova@rain.wtf>
2018-06-29 09:03:12 +0300

    add styles for ticket header

--- aviasales-demo-frontend/src/TIcketHeader.js
+++ aviasales-demo-frontend/src/TIcketHeader.js
@@ -1,58 +1,101 @@
 import React, {Component} from 'react';
+import {render} from "react-dom";
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import logologo from './img/logologo.png';
 import './TicketHeader.css';
 import styled from "styled-components";
+import 'flexboxgrid2/flexboxgrid2.css';
+import './style.css';
 
-function logoImg(props) {
-    return <img src={props.src} className={props.className} alt={props.alt}/>;
+function LogoImg(props) {
+    return <img {...props}/>;
+}
+function MainHeader(props) {
+    return <div {...props}/>;
+}
+function Buttons(props) {
+    return <button {...props} />;
 }
 
-const ButtonSearch = styled(Buttons) `
+function HeadContainer(props) {
+    return <div {...props}/>
+}
+
+function Inputs(props) {
+    return (
+        <input {...props}/>
+    )
+}
+
+
+const MainHeaderStyled = styled(MainHeader) `
+    width: 100%;
+    background-color: rgb(33, 150, 243);
+`;
+const ButtonSearch = styled(Buttons)`
     width: 194px;
     height: 58px;
     font-size: 20px;
 `;
 
+const InputsStyled = styled(Inputs) `
+    padding: 18px;
+    background-color: #fff;
+    border: 1px solid blue;
+    position: relative;
+`
 
-class MovieTexts extends React.Component {
-    constructor(props) {
-        super(props);
-        this.descr = {description: "Кто мы? Какое место мы занимаем во Вселенной? Эти вопросы стоят перед героями фильма. Экипаж космического корабля С. С. Дискавери — капитаны Дэйв Боумэн, Фрэнк Пул и их бортовой компьютер ХЭЛ-9000 — должны исследовать район галактики и понять, почему инопланетяне следят за Землей. На этом пути их ждет множество неожиданных открытий…"};
-    }
-    render(props) {
-        return (
-            {/*<p children={this.descr.description}/>*/},
-                <p {...this.props} children={this.descr.description}/>
-        )
-    }
-    // render(props) {
-    //     return <p {...this.props}/>
-    // }
+function Header(props) {
+    return(
+       <MainHeaderStyled className="col-xs-12
+       col-sm-12
+       col-md-12
+       col-lg-12">
+        <a className="logo-link" href="#">
+            <picture className="logo-container">
+                <LogoImg className="main-logo" srcSet={props.srcSet} alt={props.className} width={props.width} height={props.height}/>
+            </picture>
+            </a>
+       </MainHeaderStyled> 
+    );
 }
 
-class ToggleButton extends React.Component {
+
+class HeaderComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {toggle: none};
-        this.showText = this.showText.bind(this);
+        // this.state = {toggle: none};
+        // this.showText = this.showText.bind(this);
     }
-
-    showText() {
-        const changeText = this.state.toggle == none ? inline : none;
-        this.setState({toggle: changeText});
-    }
-
     render() {
-        return (
-            <div className="movie-content">
-                <StButtons
-                    className="btn-test"
-                    type="button"
-                    children="info"
-                    onClick={this.showText}>
-                </StButtons>
-                <MovieTexts style={{display: this.state.toggle}}/>
-            </div>
+        return (  
+            <Header srcSet= {logologo} width="auto" height="100%" />
         )
     }
 }
+export default HeaderComponent;
+// class HeaderComponent extends React.Component {
+//     constructor(props) {
+//         super(props); 
+//         this.buttonDescr={descr: "Найти билеты"};
+//         this.labelTextFrom={fromText: "Откуда"};
+//         this.labelTextTo = {toText: "Куда"};
+//     }
+//       render() {
+//         return (
+//             <div className="ticket-content">
+//                 <div className="main-logo">
+//                     <LogoImg className="main-logo__img" alt={props.alt}/>
+//                 </div>
+//                 <ButtonSearch
+//                     className="btn-search"
+//                     type="button"
+//                     children="info"
+//                     onClick={this.showText}>
+//                 </ButtonSearch>
+//                 <MovieTexts style={{display: this.state.toggle}}/>
+//             </div>
+//         )
+//     }
+// }
--- aviasales-demo-frontend/src/index.js
+++ aviasales-demo-frontend/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {render} from "react-dom";
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import HeaderComponent from './TicketHeader'
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HeaderComponent/>, document.getElementById('header'));
 registerServiceWorker();
