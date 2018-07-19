df3543fc448ccb2e82be724fdfdfe34f47adef5b
a.prostyakova <a.prostyakova@rain.wtf>
2018-07-19 19:27:24 +0300

    add input ranges

--- aviasales-demo-frontend/src/FlyTime.js
+++ aviasales-demo-frontend/src/FlyTime.js
@@ -4,14 +4,13 @@ import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 // import logo from './logo.svg';
 import styled from 'styled-components';
+import Transplantation from './Transplantation';
 import 'flexboxgrid2/flexboxgrid2.css';
 import './style.css';
 import 'normalize.css';
-import MenuHeader from './MenuHeader';
-
 
 function Price(props) {
-  return <span {...props} />;
+  return <span {...props}/>
 }
 
 const PriceStyled = styled(Price)`
@@ -24,19 +23,41 @@ function Header(props) {
   return <div {...props} />;
 }
 
-function ListItems(props) {
+
+function FlyContainer(props) {
+      <div className="flyes-container">
+          <span className="fly-item">{props.fly}</span>
+          <span className="fly-item">{props.fly2}</span>
+      </div>  
+}
+
+function Mesure(props) {
+
+}
+
+
+
+function SliderContainer(props) {
   return (
-    <div className="inputs-container">
-      <input type="checkbox" name="all" id={props.id} className={props.className} />
-      <label htmlFor={props.id} className="label-checkbox label-checkbox--transplantation-block">
-        {props.title}
-      </label>
-      <PriceStyled className="fly-cost">
-{props.titleCost}
-</PriceStyled>
-    </div>
+        <div className="flyes-container">
+          <span className="fly-item">{props.fly}</span>
+          <span className="fly-item">{props.fly2}</span>
+        </div>  
   );
 }
+
+function Arrival(props) {
+  return (
+    <div className="range-container">
+      <div className="arrives-container">
+        <span className="arrive-descr arrive-descr--from">{props.flyDescr}</span>
+        <span className="arrive-descr arrive-descr--to">{props.flyDescr2}</span>
+        <input type="range" className="fly-range fly-range--arrival-time">
+      </div>  
+    </div>
+  )
+}
+
 
 function menuLeftHeader(props) {
   return <div {...props} />;
@@ -47,7 +68,6 @@ const HeaderStyled = styled(Header)`
   color: #5B5B5C;
   width: 100%;
 `;
-
 // function MenuLeftHeader(props) {
 //   return(
 
@@ -57,13 +77,13 @@ const HeaderStyled = styled(Header)`
 function TranspanationContainer(props) {
   return (
     <Header>
-      <MenuHeader className="menu-header">{props.title}</MenuHeader>
+      <MenuHeader>{props.title}</MenuHeader>
       <div className="inputs-list">
-        <ListItems title="Все" id="all" className="input-checkbox input-checkbox--transplantation" />
+        {/* <ListItems title="Все" id="all" className="input-checkbox input-checkbox--transplantation" />  
         <ListItems title="Без пересадок" id="notransplantation" className="input-checkbox input-checkbox--transplantation" titleCost="7 712 &#8381;" />
         <ListItems title="1 пересадка" id="transplantation-1" className="input-checkbox input-checkbox--transplantation" titleCost="11 150 &#8381;" />
         <ListItems title="2 пересадки" id="transplantation-2" className="input-checkbox input-checkbox--transplantation" titleCost="16 821 &#8381;" />
-        <ListItems title="3 пересадки" id="transplantation-3" className="input-checkbox input-checkbox--transplantation" titleCost="23 986 &#8381;" />
+        <ListItems title="3 пересадки" id="transplantation-3" className="input-checkbox input-checkbox--transplantation" titleCost="23 986 &#8381;" /> */}
       </div>
     </Header>
   );
@@ -79,10 +99,10 @@ class TransplantionComponent extends React.Component {
   render() {
     return (
       <div className="col-lg-3 transpilantation-container">
-        <TranspanationContainer title="Прибытие" />
+        <TranspanationContainer title="Пересадки">
+        </TranspanationContainer>
       </div>
     );
   }
 }
 
-export default TransplantionComponent;
---
+++ aviasales-demo-frontend/src/MenuHeader.js
@@ -0,0 +1,12 @@
+import './TicketHeader.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+
+function MenuHeader(props) {
+  return <h2 {...props} />;
+}
+
+export default MenuHeader;
